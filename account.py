dd23315e47686099a88f2ed089d038b11a6fc165
kmblake24 <50854816+kmblake24@users.noreply.github.com>
2020-05-16 02:25:07 -0400

    Completed project, added page for notes

--- account.py
+++ account.py
@@ -1,60 +1,59 @@
-# Let's continue working with classes by creating 
-# a Bank Account 
-# that accepts a users name and contains a starting
-# balance of 0. 
-# Bank accounts have the ability to deposit and 
-# withdrawl funds in 
-# addition to checking if the balance is 
-# positive and if the withdrawl 
-# amount is enough. 
-# Once you've created the class,
-#  complete the requirements below.
-
 # Write an application that:
 # Prompts a user for their name
 # Prompts a user for an initial deposit
+
 # Opens an account for the user, accepting
 # their initial deposit
+
 # Prompts them to shop at an online store with three
 # potential items to purchase and an option to deposit 
 # more funds
+
 # Outputs user can't afford an item when they're 
 # account is low
-# After each purchase, prompts a user to
-# continue shopping or
-# to close their cart
 
-#from helpers import Account
+# After each purchase, prompts a user to
+# continue shopping or to close their cart
+
+from helpers import BankAccount
 
 name = input("Hello, please enter your name: ")
 
 initial = int(input('Please enter your initial deposit: '))
 
-account = []
-
-account.append(initial)
+bank = BankAccount(name)
+bank.make_deposit(initial)
 
 shopping = ''
-
 while shopping != 'q':
-    print("""Choose an item or deposit more funds")
+    print("""Choose an item or deposit more funds
     1. Buy shirt for $100
     2. Buy shoes for $200
     3. Buy pants for $40
     4. Deposit more funds
+    5. Check Balance
     """)
 
     choice = int(input("Enter your choice: "))
+    amount = 0
 
     if choice == 1:
-        account.append(100)
-        print(sum(account))
-
+        amount = 100
+        bank.withdraw(amount)
+        print(f'Your balance is {bank.check_bal()}')
     elif choice == 2:
-        print('choose 2')
+        amount = 200
+        bank.withdraw(amount)
+        print(f'Your balance is {bank.check_bal()}')
     elif choice == 3:
-        print('choose 3')
+        amount = 40
+        bank.withdraw(amount)
+        print(f'Your balance is {bank.check_bal()}')
     elif choice == 4:
-        print("choose 4")
-    
+        amount = int(input("Enter how much you want to deposit: "))
+        bank.make_deposit(amount)
+        print(f'Your balance is {bank.check_bal()}')
+    elif choice == 5:
+        print(f'Your balance is {bank.check_bal()}')
+
     shopping = input("Hit any key to continue shopping or press q to quit: ")
--- helpers.py
+++ helpers.py
@@ -1,18 +1,4 @@
-# Class will accept a username and balance
 
-# The __init__ method serves as the constructor for our class, 
-# allowing us to define attributes and the initial state of each car that gets made.
-
-# self - represents the instance of the class; by using self you can access
-# the attributes of the class; self only affects the class being used; not all instances of the
-# class
-
-# for name - the name will be passed through the class
-# class Bank_Account instantiates the class - you will need to pass the name here
-# will do class Bank_Account(self, object)
-
-# predeteming every balance will start at 0
-# have to pass in object b/c the name will be passed into the class
 
 class BankAccount(object):  
     def __init__(self, object):
@@ -20,12 +6,26 @@ class BankAccount(object):
         self.balance = 0
     # above is the constructor
 
-# test - see the attributes, see if you can pass a name
-# we intended our object to be a name; so pass it in
-# use ._dict_ to see the dictionaries attributes
+    def make_deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print('Insufficient Funds to withdraw this amount.')
+    
+    def check_bal(self):
+        return int(self.balance)
+        # if self.balance > 0:
+        #     print("You have a positive balance.")
+        # if self.balance <= 0:
+        #     print("You do not have a positive balance.")
+
+
 
 # bank = BankAccount("Kyle")
-# print(bank.__dict__)
-# outputs: {'name': 'Kyle', 'balance': 0}
-
-
+# bank.make_deposit(100)
+# bank.is_bal_pos()
+# bank.withdraw(200)
+#print(bank.__dict__)
